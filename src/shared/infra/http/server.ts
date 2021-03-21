d1a5277367603b4d8ecb34f6f75d33cb65f129d3
Maicon Lourenço <maiconloure@gmail.com>
2021-03-21 19:32:58 -0300

    add to gcloud

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,7 +2,7 @@ import app from "./app";
 import env from '../../../config/index'
 
 let host = env.HOST || 'http://localhost'
-let port = env.PORT || '8888'
+let port = env.PORT || '8080'
 
 app.server.listen(env.PORT, () => {
     console.info(`⚡️⚡️⚡️[ABSOLUT-PSYCO_API]: Server is running at ${host}:${port}`)
