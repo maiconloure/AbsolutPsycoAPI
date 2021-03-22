cc4b344c805caa931d2a9974c7fca3ae93d88659
Maicon Lourenço <maiconloure@gmail.com>
2021-03-21 23:32:54 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -21,7 +21,7 @@ class App {
 
   private middlewares(): void {
       this.express.use(express.json())
-      // this.express.use(cors())
+      this.express.use(cors())
   }
 
   private sockets(): void {
